15782e7d305edb6af7726e23a5fae22883152152
Christina Harris <christinathedesigner@outlook.com>
2020-07-10 10:31:31 -0700

    complete exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    h = {}
+    
+    for i, weight in enumerate(weights):
+        if weight in h.keys():
+            h[weight].append(i)
+        else:
+            h[weight] = [i]
+    
+    for k, v in h.items():
+        found = h.get(limit - k)
+        if found:
+            if found[0] == h[k][0]:
+                secondary = h[k][1]
+            else:
+                secondary = h[k][0]
+            return (max(found[0], secondary), min(found[0], secondary))
 
     return None
