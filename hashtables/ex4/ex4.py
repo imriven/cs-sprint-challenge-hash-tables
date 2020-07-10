f6af4b22acfde107db84240d334645e3e5b42fb0
Christina Harris <christinathedesigner@outlook.com>
2020-07-10 09:18:57 -0700

    ees4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,17 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    p = {}
+    n = {}
+    for num in a:
+        if num > 0:
+            p[num] = True
+        elif num < 0:
+            n[abs(num)] = True
+    for num in p.keys():
+        if num in n.keys():
+            result.append(num)
 
     return result
 
